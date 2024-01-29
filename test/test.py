9ce37eaa82567d2ba5b5f5a155f2dc245808fdcb
kirilman <kirilman@list.ru>
2024-01-29 18:02:27 +0500

    f

--- test/test.py
+++ test/test.py
@@ -2,9 +2,7 @@ import sys
 # sys.path.append("../")
 sys.path.append("./")
 from app.main import sum, letterxyxy2org
-import cv2
 import numpy as np
-import pandas as pd
 def test_sum():
     assert sum(5, 5) == 10
 
